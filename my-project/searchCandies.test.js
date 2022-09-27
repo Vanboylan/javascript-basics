5bf9024b81ac23df8a4fa4cee2dd170bdc1ecaef
Vanboylan <108574954+Vanboylan@users.noreply.github.com>
2022-09-27 16:18:52 +0100

    searching function for sweets

--- my-project/searchCandies.test.js
+++ my-project/searchCandies.test.js
@@ -1,7 +1,24 @@
-const searchCandies= require('./searchCandies');
+const searchCandies = require('./searchCandies');
 
 describe('searchCandies', () => {
-    it('searches for Mars and returns result', () => {
-        expect(searchCandies('Ma', 10)).toBe('')
-    })
-})
+    it('returns two results for Ma, 10', () => {
+        expect(searchCandies('Ma', 10)).toEqual(["Mars", "Maltesers"]);
+    });
+
+    it('returns one result for Ma, 2', () => {
+        expect(searchCandies('Ma', 2)).toEqual(["Mars"]);
+    });
+
+    it('returns three results for S, 10', () => {
+        expect(searchCandies('S', 10)).toEqual([ 'Skitties', 'Skittles', 'Starburst' ]);
+    });
+
+    it('returns two results for S, 4', () => {
+        expect(searchCandies('S', 4)).toEqual([ 'Skitties', 'Skittles' ]);
+    });
+
+    it('returns two results for ma, 10', () => {
+        expect(searchCandies('ma', 10)).toEqual(["Mars", "Maltesers"]);
+    });
+    
+});
